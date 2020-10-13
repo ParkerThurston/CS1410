1fa4555ae1e296de27e7dc23f04490528dd8d2c0
Parker Thurston <parker.thurston22@gmail.com>
2020-10-12 19:34:54 -0600

    Finished Coffee and 8.1

--- coffeemachine.py
+++ coffeemachine.py
@@ -14,97 +14,93 @@ class Product:
 
 class CashBox:
     def __init__(self):
-        self.credit = 0.0
-        self.total_cash = 0.0
+        self.credit = 0
+        self.total_cash = 0
         
 
     def deposit(self, amount):
-        self.total_cash += amount
+        if amount not in(5,10,25,50):
+            print("INVALID AMOUNT")
+            print("We only take half-dollars, quarters, dimes, and nickels.")
+            return
+        self.credit += amount
+        print("Depositing " +str(amount)+" cents. You Have " +str(self.credit) +" cents credit.")
 
     def return_coins(self):
-        #retAmount = self.credit
-        self.total_cash -= self.credit
-        retAmount = self.total_cash
-        return retAmount
+        print("Returning " +str(self.credit)+ " cents.")
+        self.credit = 0
+        
+      
     
     def have_you(self, amount):
-        if amount <= self.credit:
-            return True
-        else:
-            return False
+        return amount <= self.credit
 
     def deduct(self, amount):
-        self.credit -= amount
-        print("Returning " +str(self.credit)+ " cents.")
+        if self.have_you(amount):
+            self.credit -= amount
+            self.total_cash+= amount
+            if self.credit > 0:
+                print("Returning " + str(self.credit) + " cents.")
+                self.credit = 0
+        
 
     def total(self):
-        Mola = self.total_cash
-        return Mola
+        return self.total_cash
+        
 
         
 
 class Selector:
-    def __init__(self, choice):
-        self.cashBox = CashBox()
-        self.products = ["Filler","Black", 35, "White", 35, "Sweet", 35, "White & Sweet", 35, "Bouillon", 25]
-        self.recipe = {"1":"   Dispensing Cup\n   Dispensing Coffee\n   Dispensing Water", 
-                    "2":    "Dispensing Cup\n   Dispensing Coffee\n   Dispensing Creamer\n   Dispensing Water", 
-                    "3":    "Dispensing Cup\n   Dispensing Coffee\n   Dispensing Sugar\n   Dispensing Water"}
-    def select(self, choice):
-        if choice == "1" or choice == "2" or choice == "3" or choice == "4":
-            #if self.cashBox.have_you(35) == True:
-            self.product = Product(str(self.products[int(choice)]), str(self.products[int(choice)*2]), str(self.recipe[choice]))
-            print("Making " +(self.product.name)+":")
-            self.product.make()
-            self.cashBox.deduct(int(self.products[int(choice)*2]))
-            self.cashBox.return_coins()
+    def __init__(self, cashbox, products):
+        self.cashBox = cashbox
+        self.products = products
         
-        #   else:
-        #         print("F")
-        #elif choice == "5":
-        #     pass
-        else:
-            print(choice + " is not an option, Try again.")
-            
 
-        
+    def select(self, choiceIndex):
+        if choiceIndex <  1 or choiceIndex > 5:
+            print("Invalid Choice")
+            return
+        choice = self.products[choiceIndex - 1]
+        if not self.cashBox.have_you(choice.get_prices()):
+            print("Sorry. Not enough money deposited")
+            return
+        else:
+            print("Making " + choice.name + ":")
+            choice.make()
+            self.cashBox.deduct(choice.get_prices())
+
 
 class CoffeeMachine:
     def __init__(self):
         self.cashBox = CashBox()
-        self.selector = Selector(None)
+        products = []
+        products.append(Product("black", 35, "   Dispensing cup\n   Dispensing coffee\n   Dispensing water"))
+        products.append(Product("white", 35, "   Dispensing cup\n   Dispensing coffee\n   Dispensing creamer\n   Dispensing water"))
+        products.append(Product("sweet", 35, "   Dispensing cup\n   Dispensing coffee\n   Dispensing sugar\n   Dispensing water"))
+        products.append(Product("white & sweet", 35, "   Dispensing cup\n   Dispensing coffee\n   Dispensing sugar\n   Dispensing creamer\n   Dispensing water"))
+        products.append(Product("bouilon", 25, "   Dispensing cup\n   Dispensing bouillon powder\n   Dispensing water"))
+        self.selector = Selector(self.cashBox, products)
 
     def one_action(self):
         print("__________________________________________________________ \n   PRODUCT LIST: all 35 cents, except bouillon (25 cents) \n   1=black, 2=white, 3=sweet, 4=white & sweet, 5=bouillon \n   Sample commands: insert 25, select 1.")
         resp = input("Your command: ")
-
         resp = resp.split()
 
         if resp[0] == "quit":
             return False
         if resp[0] == "cancel":
-            amount = self.cashBox.return_coins()
-            print("Returning "+ str(int(amount)) +" cents.")
-            self.cashBox.credit = 0.0
+            self.cashBox.return_coins()
             self.one_action()
         elif resp[0] == "insert":
-            if resp[1] == "50" or resp[1] == "25" or resp[1] == "10" or resp[1] == "5":
-                credit = int(resp[1])
-                self.cashBox.credit += credit
-                self.cashBox.deposit(credit)
-                print("Depositing " +resp[1]+" cents. You Have " +str(int(self.cashBox.credit))+ " cents credit.")
-                self.one_action()
-            else:
-                print("INVALID AMOUNT")
+            self.cashBox.deposit(int(resp[1]))
+            self.one_action()
         elif resp[0] == "select":
-            selection = resp[1]
-            if selection == "1" or selection =="2" or selection == "3" or selection == "4" or selection == "5":
-                self.selector.select(selection)
-            else:
-                print(selection + "is not an option, try again.") 
-                self.one_action()   
+            self.selector.select(int(resp[1]))
+            self.one_action()
         else:
-            print("Invalid entry")
+            print("Invalid command.")
+            self.one_action()
+    
 
     def totalCash(self):
         totalCash = self.cashBox.total_cash
