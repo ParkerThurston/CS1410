3805d74af995091f8233a7f85e6176668ad068bd
Parker Thurston <parker.thurston22@gmail.com>
2020-11-29 18:51:21 -0700

    Project 6 finished

--- Project 6/dataplot.py
+++ Project 6/dataplot.py
@@ -1,28 +1,33 @@
+#Code written by Parker Thurston with help from Class (With some extra help from Hannah Young and John Mittelstaedt)
+
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
-import array as arr
+
 
 def analyze(fname):
-    #Read the raw data
+    # Read the raw data
     raw_data = np.loadtxt(fname)
-    #smooth raw data
+    # Smooth raw data
     smooth_data = smooth(raw_data)
     
 
-    #Find pulses
+    # Find pulses
     pulses = []
     vt = 100
+    index_p = 0
     for i in range(len(smooth_data)-2):
         if (smooth_data[i + 2] - smooth_data[i]) > vt:
-            pulses.append(i)
+            if i > index_p:
+                pulses.append(i)
 
-            #skip until the sample begins to decrease
+            # Skip until the sample begins to decrease
             i += 1
             while i < len(smooth_data) and smooth_data[i+1] > smooth_data[i]:
                 i+= 1
+                index_p = i
     
-    #plot the data
+    # Plot the data
     _,axes = plt.subplots(nrows=2)
     axes[0].plot(raw_data)
     axes[0].set(title=fname, ylabel="Raw", xticks= [])
@@ -34,32 +39,38 @@ def analyze(fname):
     plt.savefig(pdf_file)
     # Calculated area and write to file
 
-
-    # area = 0
-    # k = 0
-    # for i in range(len(pulses)-1):
-    #     while (pulses[i+k] < pulses[i+1]):
-    #         area = raw_data[i]
-    #         k+=1
-        
-    #     print( i +"    ("+area+")")
-    #     i+=1
-
-
-
-    pulse_end = pulses[-2]
+    pulse_end = pulses[-1]
     k = 0
     pulse_area = 0
+    Maxwidth=50
+    width = 0
+    pulse_areaL = []
 
-
-    for i in pulses:
-        if i != pulse_end:
+    for p in pulses:
+        if p != pulse_end:
             var = pulses[k+1]
-            while( i < var):
-                pulse_area += raw_data[i]
-                i+=1
+            while( p < var):
+                pulse_area += raw_data[p]
+                p+=1
             k+=1
-            
+            pulse_areaL.append(pulse_area)
+            pulse_area = 0
+        else:
+            while( width < Maxwidth):
+                pulse_area += raw_data[p]
+                p+=1
+                width+=1
+            pulse_areaL.append(pulse_area)
+            pulse_area = 0
+    
+    # Writing to a file 
+
+    out_file = fname[:-3] +"out"
+    with open(out_file, "w") as file1: 
+        file1.write(fname + ":     \n")
+        for L in range(len(pulses)):
+            file1.writelines("Pulse " + str(L+1) + ": " + str(pulses[L])+ "  (" + str(int(pulse_areaL[L]))+ ")\n") 
+    
     
 
 def smooth(data):
