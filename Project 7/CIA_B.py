0785ba67df1c28083fe580f0f7b23dc6989c69fa
Parker Thurston <parker.thurston22@gmail.com>
2020-12-01 21:50:42 -0700

    Project 7 :)

---
+++ Project 7/CIA_B.py
@@ -0,0 +1,41 @@
+#Code written by Parker Thurston, with help from class code (With some extra help from Hannah Young and John Mittelstaedt)
+
+from time import perf_counter
+import requests
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import cpu_count
+
+
+# Download a flag from the CIA
+prefix = 'https://www.cia.gov/library/publications/resources/the-world-factbook/graphics/flags/large/'
+    # Note: No headers={...}
+
+
+def Flags(flagLyst):
+    bitties = 0 
+    for ch in flagLyst:
+        fname = prefix + ch + "-lgflag.gif"
+        flag = requests.get(fname).content
+        file_name = "flags/" + ch + ".gif"
+        with open(file_name,"wb") as f:
+            f.write(flag)
+        bitties += len(flag)
+    return bitties
+    
+
+
+def main():
+    with open("flags.txt") as f:
+        flagL = [line.strip() for line in f]
+
+    start = perf_counter()
+    with ProcessPoolExecutor(cpu_count()) as p:
+        Fullbitties = Flags(flagL)
+    stop = perf_counter()
+    with open("Output.txt", "a") as f:
+        f.write("CIA_B: Outputs \n")
+        f.write("Elapsed Time: " + str(stop - start) + " seconds \n")
+        f.write( str(Fullbitties) + " bytes downloaded \n")
+if __name__ == "__main__":
+    main()
+
